753720c985032a4ba36f3e5f452f658c66e567c5
baqkha <85809077+baqkha@users.noreply.github.com>
2025-05-21 16:07:20 +0200

    save canvas in intro maybe

--- Assets/whiteboard/Script/VRSaveButton.cs
+++ Assets/whiteboard/Script/VRSaveButton.cs
@@ -4,16 +4,20 @@ public class VRSaveButton : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Marker"))  // Your pen object must be tagged "Marker"
+        if (other.CompareTag("Marker"))
         {
             WhiteBoard board = FindObjectOfType<WhiteBoard>();
             if (board != null)
             {
-                string fileName = "Whiteboard_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
+                string fileName = "Whiteboard_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
                 board.SaveWhiteboard(fileName);
+
+                // Save the filename so the other scene knows what to load
+                PlayerPrefs.SetString("SavedWhiteboard", fileName);
+                PlayerPrefs.Save();
             }
 
-            GetComponent<Renderer>().material.color = Color.green; // Optional feedback
+            GetComponent<Renderer>().material.color = Color.green;
         }
     }
 }
