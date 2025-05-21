611a2dfce16f10c91a4869dc3d0b42479d31d694
Herman Wigge <herman@wigge.nu>
2025-05-21 17:27:54 +0200

    Updated save Image and UI transport scene

--- Assets/whiteboard/Script/VRSaveButton.cs
+++ Assets/whiteboard/Script/VRSaveButton.cs
@@ -2,22 +2,20 @@ using UnityEngine;
 
 public class VRSaveButton : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public void saveImage()
     {
-        if (other.CompareTag("Marker"))
+        WhiteBoard board = FindObjectOfType<WhiteBoard>();
+        if (board != null)
         {
-            WhiteBoard board = FindObjectOfType<WhiteBoard>();
-            if (board != null)
-            {
-                string fileName = "Whiteboard_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
-                board.SaveWhiteboard(fileName);
+            string fileName = "Whiteboard_" + System.DateTime.Now.ToString("yyyyMMdd_HHmmss") + ".png";
+            board.SaveWhiteboard(fileName);
 
-                // Save the filename so the other scene knows what to load
-                PlayerPrefs.SetString("SavedWhiteboard", fileName);
-                PlayerPrefs.Save();
-            }
-
-            GetComponent<Renderer>().material.color = Color.green;
+            // Save the filename so the other scene knows what to load
+            PlayerPrefs.SetString("SavedWhiteboard", fileName);
+            PlayerPrefs.Save();
+            Debug.Log("Image Saved Triggered");
         }
+
+        
     }
 }
