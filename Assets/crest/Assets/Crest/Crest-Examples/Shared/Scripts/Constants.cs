2ba43ed36abff7f0dd97408440de08a4f76a2a99
Herman Wigge <herman@wigge.nu>
2025-05-25 16:12:59 +0200

    deleted example scenes

--- Assets/crest/Assets/Crest/Crest-Examples/Shared/Scripts/Constants.cs
+++
@@ -1,7 +0,0 @@
-// Crest Ocean System
-
-// This file is subject to the MIT License as seen in the root of this folder structure (LICENSE)
-
-namespace UnityEngine.InputSystem { }
-namespace UnityEngine.Rendering.HighDefinition { }
-namespace UnityEngine.Rendering.Universal { }
